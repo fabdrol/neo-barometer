bb6f7269eef0dbe5919ea1c30bb1fe21623c4c5a
Fabian Tollenaar <fabian@starting-point.nl>
2015-11-07 22:55:26 +0100

    sync

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 
   module.exports = {
     Temperature: require('./lib/Temperature'),
-    Pressure: require('./lib/Pressure')
+    Pressure: require('./lib/Pressure'),
+    Altitude: require('./lib/Altitude')
   };
 })();
