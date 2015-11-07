56cfa7b1a4639b5f6c997a08da597fc4c1fdab7d
Fabian Tollenaar <fabian@starting-point.nl>
2015-11-07 20:49:46 +0100

    sync

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
   let Bluebird = require('bluebird');
   let fs = Bluebird.promisifyAll(require('fs'));
   let debug = require('debug')('neo-barometer');
+  let RStream = require('stream').Readable;
 
   let files = {
     temp_raw: '/sys/class/i2c-dev/i2c-1/device/1-0060/iio\:device0/in_temp_raw',
@@ -13,34 +14,42 @@
   };
 
   function poll() {
-    let temperature = { scale: 0, raw: 0 };
-    let pressure = { scale: 0, raw: 0 };
+    return new Bluebird((accept, reject) => {
+      let temperature = { scale: -1, raw: -1, value: -1 };
+      let pressure = { scale: -1, raw: -1, value: -1 };
 
-    fs
-      .readFileAsync(files.temp_scale, 'utf8')
-      .then((scale) => {
-        temperature.scale = scale;
-        return fs.readFileAsync(files.pres_scale, 'utf8');
-      })
-      .then((scale) => {
-        pressure.scale = scale;
-        return fs.readFileAsync(files.temp_raw, 'utf8');
-      })
-      .then((raw) => {
-        temperature.raw = raw;
-        return fs.readFileAsync(files.pres_raw, 'utf8');
-      })
-      .then((raw) => {
-        pressure.raw = raw;
-        debug('Temperature: ' + (temperature.raw * temperature.scale));
-        debug('Pressure: ' + (pressure.raw * pressure.scale));
-      })
+      fs
+        .readFileAsync(files.temp_scale, 'utf8')
+        .then((scale) => {
+          temperature.scale = scale;
+          return fs.readFileAsync(files.pres_scale, 'utf8');
+        })
+        .then((scale) => {
+          pressure.scale = scale;
+          return fs.readFileAsync(files.temp_raw, 'utf8');
+        })
+        .then((raw) => {
+          temperature.raw = raw;
+          return fs.readFileAsync(files.pres_raw, 'utf8');
+        })
+        .then((raw) => {
+          pressure.raw = raw;
+          pressure.value = (pressure.raw * pressure.scale);
+          temperature.value = (temperature.raw * temperature.scale);
+          accept({ pressure, temperature });
+        })
 
-      .catch((err) => {
-        debug('Error:', err.message);
-      })
-    ;
+        .catch((err) => {
+          reject(err);
+        })
+      ;
+    });
   }
 
-  setInterval(poll, 1000);
+  setInterval(() => {
+    poll().then((data) => {
+      debug('Pressure: ' + data.pressure.value);
+      debug('Temperature: ' + data.temperature.value);
+    });
+  }, 1000);
 })();
