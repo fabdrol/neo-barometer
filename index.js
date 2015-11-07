7a6f64f8b16f12648166b9d789b0a98e2ce99abe
Fabian Tollenaar <fabian@starting-point.nl>
2015-11-07 20:40:00 +0100

    sync

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 (function() {
   'use strict';
-
-  let fs = Bluebird.promisifyAll(require('fs'));
+  
   let Bluebird = require('bluebird');
+  let fs = Bluebird.promisifyAll(require('fs'));
 
   let files = {
     temp_raw: '/sys/class/i2c-dev/i2c-1/device/1-0060/iio\:device0/in_temp_raw',
@@ -10,7 +10,7 @@
     pres_raw: '/sys/class/i2c-dev/i2c-1/device/1-0060/iio\:device0/in_pressure_raw',
     pres_scale: '/sys/class/i2c-dev/i2c-1/device/1-0060/iio\:device0/in_pressure_scale'
   };
-
+  
   function poll() {
     let temperature = { scale: 0, raw: 0 };
     let pressure = { scale: 0, raw: 0 };
